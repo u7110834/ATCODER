089d9d5f952290ff95300b88a484cb4ad7e7c2ef
u7110834 <u7110834@gmail.com>
2023-11-16 13:46:34 +1100

    added txt files to algorithms, renamed expriments to experiemnt

--- experiments/modint.cpp
+++ experiments/modint.cpp
@@ -21,13 +21,31 @@ template <typename T> inline bool chmax(T& a, const T& b) {bool compare = a < b;
 
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define rep1(i, n) for (int i = 1; i <= (int)(n); i++)
-#define ll; long long
+#define ll long long
 
 auto putline = [](string s = "========"){
     cout << s << endl;
 };
 
+const int MOD = 1000000007;
+
+long long mod(long long val, long long m) {
+  long long res = val % m;
+  if (res < 0) res += m;
+  return res;
+}
+
 int main()
 {   
-
+    long long a, b, c;
+  a = 111111111;
+  b = 123456789;
+  c = 987654321;
+  cout << "Overflow : " << a * b * c % MOD << endl;       // a*b*c がオーバーフローするので間違った答えに
+  cout << "Correct : " << a * b % MOD * c % MOD << endl; // a*b で一旦 MOD をとる
+  
+  //mod of negative int 
+  cout << "17 mod 5 : " << 17 %5 << endl;
+  cout << "-17 mod 5 : " << -17%5 << endl;
+  cout << "correct -17 mod 5 : " << mod(-17,5) << endl;
 }
